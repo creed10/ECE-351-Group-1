6efdf99a3ea43193f98f4a4b48d0b7142ad33d56
CartographerR <rpauly110@gmail.com>
2019-04-19 14:02:55 -0400

    Workspace3 folder contains everything necessary to program/code the FPGA in the Xilinx SDK.

---
+++ Workspace3/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_myhygro3.elf_on_local.tcl
@@ -0,0 +1,7 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183698774A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183698774A"} -index 0
+dow C:/Users/Ryan/.Xilinx/SDK/2017.4/Workspace3/myHYGRO3/Debug/myHYGRO3.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Basys3 210183698774A"} -index 0
+con
